5c6e205d85a613c6b9f9d55770c9f2c15b11d78e
cvvishnuu <cvishnuu01@gmail.com>
2020-10-02 15:18:47 +0530

    Applying changes to navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,39 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 import './Navigation.css';  
  
-class Navigation extends Component {
-   constructor(props) {
-      super(props);
-       this.state = {
-           value:'',
-       }
-   }
-   
-   onRouteChange = () => {
-       if(window.location.pathname === "/business/signin"){
-        this.setState({value:" | LOGIN"})
-       } else {
-        this.setState({value:" | REGISTER"})
-       }
-    }
-   
-    render() {
-        return (
-            <nav className = "navigation">
-               <h4 className = 'logo'>MINIMATO</h4>
-                <ul className = "nav-links">
-                    <li onClick={this.onRouteChange}>
-                        <Link to = "/business/signin" style = {{position:"fixed", right: 200, top: 20}}>LOGIN</Link>
-                    </li>
-                    <li onClick={this.onRouteChange} > 
-                        <Link to = '/business/signup' style = {{position:"fixed", top: 20, right: 50}}>REGISTER</Link>
-                    </li>
-                </ul>
-            </nav>
-        )    
-    }
-    
+const Navigation = () => {
+    return (
+        <nav className = "navigation">
+            <h4 className = 'logo'>MINIMATO</h4>
+            <ul className = "nav-links">
+                <li >
+                    <Link to = "/business/signin" style = {{position:"fixed", right: 200, top: 20}}>LOGIN</Link>
+                 </li>
+                <li > 
+                    <Link to = '/business/signup' style = {{position:"fixed", top: 20, right: 50}}>REGISTER</Link>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navigation;
